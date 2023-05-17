8b6ca27806430d51191f9a931234312d5d4eca06
Hannes Winkler <hanneswinkler2000@web.de>
2023-05-17 23:14:17 +0200

    regenerate linux & macos native code, add windows native code

--- plano/macos/Runner/MainFlutterWindow.swift
+++ plano/macos/Runner/MainFlutterWindow.swift
@@ -3,12 +3,9 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
-    var windowFrame = self.frame
+    let flutterViewController = FlutterViewController()
+    let windowFrame = self.frame
     self.contentViewController = flutterViewController
-
-    // Includes 30px for Macos Title Bar
-    windowFrame.size = CGSize.init(width: 1280, height: 830)
     self.setFrame(windowFrame, display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
